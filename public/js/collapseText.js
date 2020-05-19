81c3da7b9b4a8acb511082c92709372e3e2b6d43
Ant Iva <sg.antiiva@gmail.com>
2020-05-19 11:31:00 +0300

    collapse text complete

---
+++ public/js/collapseText.js
@@ -0,0 +1,45 @@
+const recipeContainerElem = document.getElementById("recipe-container");
+const recipeTextElem = document.getElementById("recipe-text");
+
+const readMoreBut = document.createElement("button");
+
+
+const collapseBut = document.createElement("button");
+
+
+const recipeText = recipeTextElem.textContent;
+const recipeTextShort = trancateString(recipeText, 1050);
+
+function trancateString(string, length) {
+    if (string.length > length) return string.slice(0, length);
+    else return string;
+}
+
+function createUI() {
+    readMoreBut.innerHTML = "Read More...";
+    readMoreBut.classList.add("btn", "btn-link");
+    collapseBut.innerHTML = "Collapse Text";
+    collapseBut.classList.add("btn", "btn-link");
+
+    readMoreBut.addEventListener('click', () => {
+        recipeTextElem.textContent = recipeText;
+        recipeContainerElem.removeChild(readMoreBut);
+        recipeContainerElem.appendChild(collapseBut);
+    });
+
+    collapseBut.addEventListener('click', () => {
+        recipeTextElem.textContent = recipeTextShort;
+        recipeContainerElem.removeChild(collapseBut);
+        recipeContainerElem.appendChild(readMoreBut);
+    });
+}
+
+function collapseText() {
+    createUI();
+    if (recipeText.length > 1050) {
+        recipeTextElem.textContent = recipeTextShort;
+        recipeContainerElem.appendChild(readMoreBut);
+    }
+}
+
+collapseText();
