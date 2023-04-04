befc967b71503095b90bf9e55127965b38627334
Sergiy Ivanov <sg.antiiva@gmail.com>
2023-04-04 17:59:25 +0300

    template styling and favicon

--- public/js/counter.js
+++ public/js/counter.js
@@ -1,30 +1,31 @@
 'use strict';
 
 const counter = {
-    quantity: () => {
-        let qtyWrap = document.querySelectorAll('.qty-wrap');
-        if (qtyWrap.length > 0) {
-            for (let qty of qtyWrap) {
-                let minus = qty.querySelector('.qty-minus');
-                let plus = qty.querySelector('.qty-plus');
-                let input = qty.querySelector('.qty');
-                let value = parseInt(input.value, 10);
+	quantity: () => {
+		let qtyWrap = document.querySelectorAll('.qty-wrap');
+		if (qtyWrap.length > 0) {
+			for (let qty of qtyWrap) {
+				let minus = qty.querySelector('.qty-minus');
+				let plus = qty.querySelector('.qty-plus');
+				let input = qty.querySelector('.qty');
+				let value = parseInt(input.value, 10);
 
-                plus.addEventListener('click', () => {
-                    value = value + 1;
-                    input.value = value;
-                });
+				plus.addEventListener('click', () => {
+					value = value + 1;
+					input.value = value;
+				});
 
-                minus.addEventListener('click', () => {
-                    value = (value > 1) ? (value - 1) : 1;
-                    input.value = value;
-                });
+				minus.addEventListener('click', () => {
+					value = (value > 1) ? (value - 1) : 1;
+					input.value = value;
+				});
 
-            }
-        }
-    }
+			}
+		}
+	}
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    counter.quantity();
+	counter.quantity();
+	console.log("counter connected!");
 });
---
+++ readme.md
@@ -0,0 +1,4 @@
+# MealDB API web app
+
+Server-side rendering web application with a shopping cart and customer rating system.
+Tech stack: Node.js/Express.js, MongoDB and EJS.
